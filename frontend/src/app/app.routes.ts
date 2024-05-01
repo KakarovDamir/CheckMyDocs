d3e8a8d929b8649199169ba9dd7e62c4062120f2
Damir <kdamir2004@gmail.com>
2024-05-01 19:54:22 +0500

    fixes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { DocumentComponent } from './document/document.component';
+import { DocumentsComponent } from './documents/documents.component';
 import { FaqsComponent } from './faqs/faqs.component';
 import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
-    {path: 'document', component: DocumentComponent},
+    {path: 'documents', component: DocumentsComponent},
     {path: 'faqs', component: FaqsComponent},
     {path: 'login', component: LoginComponent},
     {path: '**', redirectTo: 'home'}
