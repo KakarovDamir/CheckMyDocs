763333b038e6fbc929a2cbd320d6fab58869e3cf
Zhassulan1 <zhkainazarov@gmail.com>
2024-05-01 13:03:47 +0500

    changed gitignore

--- backend/verification/api/models.py
+++ backend/verification/api/models.py
@@ -3,3 +3,5 @@ from django.db import models
 class UploadedFile(models.Model):
     file = models.FileField(upload_to='uploads/%Y/%m/%d/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
+    file_type = models.CharField(max_length=255)
+    doc_type = models.CharField(max_length=255)
