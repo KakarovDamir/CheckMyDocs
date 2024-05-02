160dd9d88ebcb03227c6cebbb9575c39edd2dd5a
Damir <kdamir2004@gmail.com>
2024-05-02 23:55:22 +0500

    fixed doc component

--- frontend/src/app/documents/documents.component.ts
+++ frontend/src/app/documents/documents.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { UploadService } from '../upload.service';
-import { Docs } from '../models';
 import { LanguageService } from '../services/language.service';
 
 @Component({
@@ -16,14 +15,16 @@ export class DocumentsComponent {
   selectedFile!: File | null;
   selectedValue: string = '';
   accept: string = '';
-  doc!: Docs;
   selectedLanguage: string = 'rus';
   user_output: string = '';
-  id!: number;
 
   uploadSuccess: boolean = false;
   uploadError: boolean = false;
-  uploadProblem: boolean = false;
+  uploadExpired: boolean = false;
+
+  docInvalidCredentials: boolean = false;
+  docWrongFormat:  boolean = false;
+  docTextNotRecognizeable:  boolean = false;
 
   constructor(
     private uploadService: UploadService,
@@ -33,26 +34,14 @@ export class DocumentsComponent {
   ngOnInit(): void {
     this.languageService.getSelectedLanguage().subscribe(language => {
       this.selectedLanguage = language;
-      if(language === 'rus'){
-        this.id = 1;
-      }
-      else if(language === 'en'){
-        this.id = 2;
-      }
-      else{
-        this.id = 3;
-      }
-      console.log(this.id);
     });
   }
 
   onPDF(){
     this.accept = '.pdf';
-    // this.doc.file_type = this.accept;
   }
   onImage(){
     this.accept = '.png';
-    // this.doc.file_type = this.accept;
   }
 
   onFileSelected(event: any): void {
@@ -67,30 +56,53 @@ export class DocumentsComponent {
     if (this.selectedFile) {  
       this.uploadService.uploadFile(this.selectedFile, this.accept, this.selectedValue)
         .then(response => {
-          if(response.is_uploaded === true){
-            if(response.is_valid === true){
-              this.uploadSuccess = true;
-              this.uploadError = false;
-              this.uploadProblem = false;
-            }
-            else{
-              if (response.message === "expired"){
-                this.uploadError = false;
-                this.uploadSuccess = false;
-                this.uploadProblem = true;
-              }
-            }
-          }else{
+          if (response.message === "expired"){
+            this.uploadError = false;
+            this.uploadSuccess = false;
+            this.uploadExpired = true;
+            this.docInvalidCredentials = false;
+            this.docWrongFormat = false;
+            this.docTextNotRecognizeable = false;
+          }else if (response.message === "valid"){
+            this.uploadError = false;
+            this.uploadSuccess = true;
+            this.uploadExpired = false;
+            this.docInvalidCredentials = false;
+            this.docWrongFormat = false;
+            this.docTextNotRecognizeable = false;
+          }else if (response.message === "invalid_credentials"){
+            this.uploadError = false;
+            this.uploadSuccess = false;
+            this.uploadExpired = false;
+            this.docInvalidCredentials = true;
+            this.docWrongFormat = false;
+            this.docTextNotRecognizeable = false;
+          }else if(response.message === "wrong_format"){
+            this.uploadError = false;
+            this.uploadSuccess = false;
+            this.uploadExpired = false;
+            this.docInvalidCredentials = false;
+            this.docWrongFormat = true;
+            this.docTextNotRecognizeable = false;
+          }else if(response.message === "text_not_recognizeable"){
+            this.uploadError = false;
+            this.uploadSuccess = false;
+            this.uploadExpired = false;
+            this.docInvalidCredentials = false;
+            this.docWrongFormat = false;
+            this.docTextNotRecognizeable = true;
+          }
+          else{
             this.uploadError = true;
             this.uploadSuccess = false;
-            this.uploadProblem = false;
+            this.uploadExpired = false;
           }
           console.log(response.is_uploaded, response.is_valid, response.message);
         })
     }else{
       this.uploadError = true;
       this.uploadSuccess = false;
-      this.uploadProblem = false;
+      this.uploadExpired = false;
     }
     this.selectedFile = null;
     this.selectedValue = '';
